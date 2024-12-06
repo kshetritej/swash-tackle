e1c09ddb7fb63517763690f4fa85a1882d6cf905
kshetritej <tejghartikshetri@gmail.com>
2024-12-06 20:27:38 +0545

    patch: prompt to more accuracy

--- be/src/gemini.ts
+++ be/src/gemini.ts
@@ -4,21 +4,32 @@ import { api_key } from "./env.config";
 const genAI = new GoogleGenerativeAI(api_key as string);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 console.log(api_key);
-const prompt =(
-  `This prompt is coming  from a API request, 
+export async function getResult(schema: string, item:string) {
+  const prompt =
+    `This prompt is coming  from a API request, 
   You don't need to explain anything just translate the given json format resembling realistic item in real world,
   and generate a response in json format resembling the response of the API. Again don't explain anything not even a little bit, 
-  only return json only in response so that can be mapped in frontend of the application using this API.
-  Generate Realistic API request for this json schema :\
-` +
-  JSON.stringify({
-    categoryId: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-    name: "string",
-    description: "string",
-  })
-);
-export async function getResult() {
-  const result = await model.generateContent(prompt);
-  console.log(result.response.text());
-}
+  only return json  in response so that can be mapped in frontend of the application using this API. 
+  The response should not contain even a single unnecessary or more letter, if the provided json is in this format:
+  "{
+  "name": "string",
+  "description": "string"
+}", 
+the response should be in this format:
+"{
+  "name":"Beer",
+  "description":"A refreshing drink made from malted grains, hops, and yeast."
+}"
 
+And this is just for the refererence the given json schema is related to ${item}
+Only these much no more no less, don't explain anything not even a little bit and don't decorate or anything just only this,
+Also don't put inside code block you don't need to put in inside or anything I should be able to directly parse the response you send using JSON.parse() function.
+Any extra response you send will cause the error.
+  Generate Realistic API request for this json schema :\
+` + schema;
+  const result = await model.generateContent(prompt);
+  console.log({
+    "result": result.response.text(),
+  })
+  return JSON.parse(result.response.text());
+}
--- be/src/server.ts
+++ be/src/server.ts
@@ -1,13 +1,21 @@
-import express, {Request, Response} from 'express'
-import { port } from './env.config';
+import express, { Request, Response } from "express";
+import { port } from "./env.config";
+import { getResult } from "./gemini";
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hi!, How can I help you?');
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "Hi!, How can I help you?" });
+});
+
+app.post("/ask", async (req: Request, res: Response) => {
+  const schema = JSON.stringify(req.body.schema);
+  const item = req.body.item;
+  const result = await getResult(schema,item );
+  res.json(result);
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
