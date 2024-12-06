300bf7da20de441e5b7c0ff50453eac600455747
kshetritej <tejghartikshetri@gmail.com>
2024-12-06 21:13:52 +0545

    perfect as of now results

--- be/src/gemini.ts
+++ be/src/gemini.ts
@@ -3,7 +3,6 @@ import { api_key } from "./env.config";
 
 const genAI = new GoogleGenerativeAI(api_key as string);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-console.log(api_key);
 export async function getResult(schema: string, item:string) {
   const prompt =
     `This prompt is coming  from a API request, 
@@ -19,9 +18,31 @@ the response should be in this format:
 "{
   "name":"Beer",
   "description":"A refreshing drink made from malted grains, hops, and yeast."
+}" and if given as in this format:
+"{
+  "notificationId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+  "notificationType": 0,
+  "notificationContext": 0,
+  "notificationContent": "string",
+  "notificationTitle": "string",
+  "notificationDate": "2024-12-06T14:42:59.782Z",
+  "userId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
 }"
+response can be in this format: 
+"{
+  "notificationId":"3fa85f64-5717-4562-b3fc-2c963f66afa6",
+  "notificationType":0,
+  "notificationContext":0,
+  "notificationContent":"Hurry Up! This is the last day to save your money!",
+  "notificationTitle":"Black Friday Sale! - 60% Off!",
+  "notificationDate":"2024-12-06T14:42:59.782Z",
+  "userId":"3fa85f64-5717-4562-b3fc-2c963f66afa6"
+}"
+this may give you the context, and I hope you understand what I am saying.
+==And this is just for the refererence the given json schema is related to ${item}==
+you should omit the above line inside "== ==" and figure out yourself if you don't have anything after "to" in above line.
 
-And this is just for the refererence the given json schema is related to ${item}
+You should not omit or add more in the keys of the schema, provide everything that is in the given schema and add nothing more than that.
 Only these much no more no less, don't explain anything not even a little bit and don't decorate or anything just only this,
 Also don't put inside code block you don't need to put in inside or anything I should be able to directly parse the response you send using JSON.parse() function.
 Any extra response you send will cause the error.
@@ -31,5 +52,5 @@ Any extra response you send will cause the error.
   console.log({
     "result": result.response.text(),
   })
-  return JSON.parse(result.response.text());
+  return result.response.text();
 }
--- be/src/server.ts
+++ be/src/server.ts
@@ -1,17 +1,21 @@
 import express, { Request, Response } from "express";
 import { port } from "./env.config";
 import { getResult } from "./gemini";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
-
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Hi!, How can I help you?" });
 });
 
 app.post("/ask", async (req: Request, res: Response) => {
-  const schema = JSON.stringify(req.body.schema);
-  const item = req.body.item;
+  const schema = JSON.stringify(req.body.prompt);
+  const item = req.body.about;
   const result = await getResult(schema,item );
   res.json(result);
 });
